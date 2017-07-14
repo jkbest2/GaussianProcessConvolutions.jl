84bbd266cbdaca13b3111ae00fbf1eb17088c8d0
John Best <jkbest@gmail.com>
2017-07-14 16:37:40 -0700

    Add . to exp in kernel.

--- src/ConvolutionKernels.jl
+++ src/ConvolutionKernels.jl
@@ -14,6 +14,6 @@ function conv_wt(kern::SquaredExponentialKernel, d::Array)
     if size(d, 1) != dim(kern)
         throw(DimensionMismatch)
     end
-    exp(-0.5 * invquad(kern.Σ, d))
+    exp.(-0.5 * invquad(kern.Σ, d))
 end
 
